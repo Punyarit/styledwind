c5bc5c8ff0d17d838c2a67c1a0dc71b3c5c1bb9d
Kwang <punyarit.thi@hlabconsulting.com>
2023-07-14 13:25:52 +0700

    deprecate: @dynamic directive

--- packages/styled/src/directives/directives.ts
+++ packages/styled/src/directives/directives.ts
@@ -5,11 +5,9 @@ import { keyframeFn } from './keyframeFn';
 import { templateFn } from './templateFn';
 import { variableFn } from './variableFn';
 import { screenFn } from './screenFn';
-import { dynamicFn } from './dynamicFn';
 
 export const directives: Record<string, Function> = {
   class: classFn,
-  dynamic: dynamicFn,
   keyframe: keyframeFn,
   join: joinFn,
   scope: scopeFn,
